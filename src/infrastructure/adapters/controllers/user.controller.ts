ca317d8c464a9c1d444ae808a038a4d8192d106d
Pablo Albaladejo <pablo@hireful.co.uk>
2024-08-29 13:32:25 +0200

    depency injection refactor

--- src/infrastructure/adapters/controllers/user.controller.ts
+++ src/infrastructure/adapters/controllers/user.controller.ts
@@ -1,26 +1,21 @@
 import { Router } from 'express';
 import { UserService } from '../../../application/services/user.service';
-import { InMemoryUserRepository } from '../repositories/inmemory-user.repository';
-import { UserRepository } from '../../../application/ports/user.repository';
 import { CompanyService } from '../../../application/services/company.service';
-import { InMemoryCompanyRepository } from '../repositories/inmemory-company.repository';
-import { CompanyRepository } from '../../../application/ports/company.repository';
 
-const router = Router();
+export function createUserRouter(
+  userService: UserService,
+  companyService: CompanyService
+): Router {
+  const router = Router();
 
-const companyRepository: CompanyRepository = new InMemoryCompanyRepository();
-const companyService = new CompanyService(companyRepository);
+  router.get('/:id', async (req, res) => {
+    try {
+      const user = await userService.getUserById(req.params.id);
+      res.json(user);
+    } catch (error) {
+      res.status(500).send('Error fetching user');
+    }
+  });
 
-const userRepository: UserRepository = new InMemoryUserRepository();
-const userService = new UserService(userRepository, companyService);
-
-router.get('/:id', async (req, res) => {
-  try {
-    const user = await userService.getUserById (req.params.id);
-    res.json(user);
-  } catch (error) {
-    res.status(500).send('Error fetching user');
-  }
-});
-
-export default router;
+  return router;
+}
--- src/infrastructure/server.ts
+++ src/infrastructure/server.ts
@@ -1,7 +1,18 @@
-import express, { Request, Response } from 'express';
-import userRoutes from './adapters/controllers/user.controller';
+import express from 'express';
+import { createUserRouter } from './adapters/controllers/user.controller';
+import { InMemoryUserRepository } from './adapters/repositories/inmemory-user.repository';
+import { InMemoryCompanyRepository } from './adapters/repositories/inmemory-company.repository';
+import { UserService } from '../application/services/user.service';
+import { CompanyService } from '../application/services/company.service';
 
 const app = express();
 
-app.use('/users', userRoutes);
+const companyRepository = new InMemoryCompanyRepository();
+const companyService = new CompanyService(companyRepository);
+
+const userRepository = new InMemoryUserRepository();
+const userService = new UserService(userRepository, companyService);
+
+app.use('/users', createUserRouter(userService, companyService));
+
 export default app;
